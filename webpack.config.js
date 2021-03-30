1705de2eff7b6eb2c9c18a46219d0605c1bfb59c
Z.Vlad <v.zui@yandex.ru>
2021-03-30 21:47:13 +0300

    Создал gitигноре

--- webpack.config.js
+++ webpack.config.js
@@ -4,56 +4,57 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 
 module.exports = {
-        entry: {
-            app: defPath + 'js/App.js'
-        },
-        output: {
-            filename: "[name].js",
-            path: path.resolve(__dirname, 'build/web/work/js/webpack'),
-        },
-        plugins: [new MiniCssExtractPlugin({
-            filename: 'css/[name].css',
-        })],
-        module: {
-            rules: [{
-                    test: /\.m?js$/,
-                    exclude: /(node_modules|bower_components)/,
-                    use: {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['@babel/preset-env']
-                        }
+    entry: {
+        app: defPath + 'js/App.js'
+    },
+    output: {
+        filename: "[name].js",
+        path: path.resolve(__dirname, 'build/web/work/js/webpack'),
+    },
+    plugins: [new MiniCssExtractPlugin({
+        filename: 'css/[name].css',
+    })],
+    module: {
+        rules: [{
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
                     }
-                },
-                {
-                    test: /\.css$/i,
-                    use: [{
-                            loader: MiniCssExtractPlugin.loader,
-                            options: {
-                                publicPath: path.resolve(__dirname, './build/web/work/style/webpack'),
-                            },
-                        },
-                        "css-loader",
-                    },
-                    {
-                        test: /\.less$/i,
-                        use: [{
-                                loader: MiniCssExtractPlugin.loader,
-                                options: {
-                                    publicPath: path.resolve(__dirname, './build/web/work/style/webpack'),
-                                },
-                            },
-                            "css-loader",
-                            "less-loader",
-                        ],
-                    },
-                    {
-                        test: /\.(woff(2)?|ttf|eot|svg|png|jpg)(\?v=\d+\.\d+\.\d+)?$/,
-                        loader: 'url-loader',
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: [{
+                        loader: MiniCssExtractPlugin.loader,
                         options: {
-                            name: '[name].[ext]'
-                        }
+                            publicPath: path.resolve(__dirname, './build/web/work/style/webpack'),
+                        },
                     },
+                    "css-loader",
                 ]
-            }
-        };
+            },
+            {
+                test: /\.less$/i,
+                use: [{
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: path.resolve(__dirname, './build/web/work/style/webpack'),
+                        },
+                    },
+                    "css-loader",
+                    "less-loader",
+                ],
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg|png|jpg)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url-loader',
+                options: {
+                    name: '[name].[ext]'
+                }
+            },
+        ]
+    }
+};
